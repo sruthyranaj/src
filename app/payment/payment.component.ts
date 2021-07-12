5c213597bac203339285f5cc7107e4f2d7b15e76
sruthy <sruthy@techversantinfo.com>
2021-07-12 09:05:37 +0530

    Added payment option for processing customer payment

--- app/payment/payment.component.ts
+++ app/payment/payment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-payment',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
-
-  constructor() { }
-
+  link: string
+  error_message;
+  amount;
+  constructor(private apiService: ApiService, private route: ActivatedRoute, private _flashMessagesService: FlashMessagesService) {this.route.params.subscribe( params => this.link = params["link"] )}
+  
   ngOnInit() {
+    this.apiService.get_api('customers/'+this.link, false)
+         .subscribe(resp => {
+         let respone = JSON.stringify(resp)
+         if(respone.length > 0){
+           console.log("respose", respone)
+           this.amount = resp['body'][0]["amount"]
+         }else{
+           this.error_message = "The link is invaild or expired"
+         }
+         });
   }
 
 }
