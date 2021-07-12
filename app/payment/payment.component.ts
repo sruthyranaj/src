289e78ddbb4e2cf64f0f6e8605c10c532896fcf3
sruthy <sruthy@techversantinfo.com>
2021-07-12 12:00:32 +0530

    stripe payment methods added

--- app/payment/payment.component.ts
+++ app/payment/payment.component.ts
@@ -12,19 +12,52 @@ export class PaymentComponent implements OnInit {
   link: string
   error_message;
   amount;
-  constructor(private apiService: ApiService, private route: ActivatedRoute, private _flashMessagesService: FlashMessagesService) {this.route.params.subscribe( params => this.link = params["link"] )}
-  
+  constructor(private apiService: ApiService, private route: ActivatedRoute, private _flashMessagesService: FlashMessagesService) { this.route.params.subscribe(params => this.link = params["link"]) }
+
   ngOnInit() {
-    this.apiService.get_api('customers/'+this.link, false)
-         .subscribe(resp => {
-         let respone = JSON.stringify(resp)
-         if(respone.length > 0){
-           console.log("respose", respone)
-           this.amount = resp['body'][0]["amount"]
-         }else{
-           this.error_message = "The link is invaild or expired"
-         }
-         });
+    this.apiService.get_api('customers/' + this.link, false)
+      .subscribe(resp => {
+        let respone = JSON.stringify(resp)
+        if (respone.length > 0) {
+          console.log("respose", respone)
+          this.amount = resp['body'][0]["amount"]
+          this.loadStripe();
+        } else {
+          this.error_message = "The link is invaild or expired"
+        }
+      });
+  }
+
+  pay(amount) {
+    // method for processing stripe payment
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token)
+        alert('Token Created!!');
+      }
+    });
+
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: amount * 100
+    });
+
+  }
+
+  loadStripe() {
+
+    if (!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement("script");
+      s.id = "stripe-script";
+      s.type = "text/javascript";
+      s.src = "https://checkout.stripe.com/checkout.js";
+      window.document.body.appendChild(s);
+    }
   }
 
 }
